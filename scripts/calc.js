05f4a045b205d6f7829b63a462cea26d81038440
Vasiliy Dikun <wasyldykun@gmail.com>
2017-01-19 21:59:57 +0300

    Caclulator - v0.1

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,3 +1,43 @@
-/**
- * Created by Spyro on 19.01.2017.
- */
+/*Найпростіший скрипт калькулятора
+* він видає діаголові вікна, в яких
+* запрошуе два числа і оператор над ними*/
+function startCalc() {
+    var firstValue;
+    var secondValue;
+    var operator;
+    var result;
+
+    firstValue = prompt("Введіть перше число", "0");
+    secondValue = prompt("Введіть друге число", "0");
+    operator = getOperator();
+    if (operator == null) {
+        alert("Введено невірний оператор. Завершення роботи.");
+        return;
+    }
+    result = mathValues(firstValue, secondValue, operator);
+    alert("Результат роботи:" +
+        "\n" + firstValue + " " + operator + " " + secondValue + " = " + result);
+
+    function getOperator() {
+        var tempValue;
+        const operators = ["+", "-", "/", "*"];
+
+        tempValue = prompt("Введіть оператор:" +
+                "\n\"+\" - додавання" +
+                "\n\"-\" - віднімання" +
+                "\n\"/\" - ділення" +
+                "\n\"*\" - множення", "");
+        if (operators.some(v => v == tempValue)) return tempValue;
+        else return null;
+    }
+
+    function mathValues(fArg, sArg, operator) {
+        switch (operator) {
+            case "+": return +fArg + +sArg;
+            case "-": return fArg - sArg;
+            case "/": return fArg / sArg;
+            case "*": return fArg * sArg;
+            default: return null;
+        }
+    }
+}
